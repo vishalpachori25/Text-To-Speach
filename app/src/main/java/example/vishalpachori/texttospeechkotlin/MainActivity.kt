e90cc196931de1c6716d171dd305e5aba5640361
Vishal Pachori <vishalpachori25@gmail.com>
2018-02-12 12:57:46 +0530

    Speech to text

--- app/src/main/java/example/vishalpachori/texttospeechkotlin/MainActivity.kt
+++ app/src/main/java/example/vishalpachori/texttospeechkotlin/MainActivity.kt
@@ -1,22 +1,27 @@
 package example.vishalpachori.texttospeechkotlin
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.speech.tts.TextToSpeech
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Button
-import android.widget.EditText
-
+import android.widget.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import java.util.*
+
 
 class MainActivity : AppCompatActivity(),TextToSpeech.OnInitListener {
     private var tts: TextToSpeech? = null
     private var buttonSpeak: Button? = null
     private var editText: EditText? = null
+    private var txtSpeechInput:TextView? = null
+	private var btn_Speak: ImageButton?=null
+	private var REQ_CODE: Int = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,19 +33,63 @@ class MainActivity : AppCompatActivity(),TextToSpeech.OnInitListener {
 //                    .setAction("Action", null).show()
 //        }
 
+        txtSpeechInput =  this.tv_speech
+		btn_Speak =  this.btn_speak
+
         buttonSpeak = this.button_speak
         editText = this.edittext_input
 
-        buttonSpeak!!.isEnabled = false;
+        buttonSpeak!!.isEnabled = false
         tts = TextToSpeech(this, this)
 
         buttonSpeak!!.setOnClickListener { speakOut() }
+
+        btn_Speak!!.setOnClickListener{
+            promptSpeechInput()
+        }
     }
+
+     private fun promptSpeechInput() {
+		val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+
+		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+
+		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+
+		intent.putExtra(RecognizerIntent.EXTRA_PROMPT, getString(R.string.speech_prompt))
+
+		try {
+			startActivityForResult(intent, REQ_CODE)
+
+		} catch ( a : Exception) {
+			Toast.makeText(applicationContext, getString(R.string.speech_not_supported),Toast.LENGTH_SHORT).show()
+		}
+	}
+
+    /**
+     * Receiving speech input
+     * */
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            REQ_CODE -> {
+                if (resultCode == RESULT_OK) {
+
+
+                    val result = data!!.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                    txtSpeechInput!!.text = result[0]
+                }
+
+            }
+        }
+    }
+
+
     override fun onInit(status: Int) {
 
         if (status == TextToSpeech.SUCCESS) {
             // set US English as language for tts
-            val result = tts!!.setLanguage(Locale.US)
+            val result = tts!!.setLanguage(Locale.ENGLISH)
 
             if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
                 Log.e("TTS","The Language specified is not supported!")
